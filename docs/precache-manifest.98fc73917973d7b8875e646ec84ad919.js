dc17f43fa512ad26786d37f1c9c71b50ef1e66ac
AiaRup <aiale88@gmail.com>
2019-06-30 21:39:00 +0200

    update docs folder

---
+++ docs/precache-manifest.98fc73917973d7b8875e646ec84ad919.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cba2f07c20564650c655eafa077dc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af733e2df608119b4284",
+    "url": "/static/css/main.a9179cb6.chunk.css"
+  },
+  {
+    "revision": "04837bc406e912f8c13b",
+    "url": "/static/js/2.c89ded8c.chunk.js"
+  },
+  {
+    "revision": "af733e2df608119b4284",
+    "url": "/static/js/main.2ff8e390.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
