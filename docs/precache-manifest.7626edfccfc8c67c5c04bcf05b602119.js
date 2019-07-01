0213e8bb04065149c4ccc4013dc1093566f53965
AiaRup <aiale88@gmail.com>
2019-07-01 07:17:07 +0200

    update docs folder

---
+++ docs/precache-manifest.7626edfccfc8c67c5c04bcf05b602119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330e2ed8e7eea126ff7a09e2b7e8ff44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af733e2df608119b4284",
+    "url": "./static/css/main.a9179cb6.chunk.css"
+  },
+  {
+    "revision": "04837bc406e912f8c13b",
+    "url": "./static/js/2.c89ded8c.chunk.js"
+  },
+  {
+    "revision": "af733e2df608119b4284",
+    "url": "./static/js/main.2ff8e390.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
